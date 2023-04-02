9edae3e97b3569daa41c0da8937a6c7a87d99a16
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-02 23:59:52 +0500

    FEAT: Successfully able to play riv animation in nextjs13 app

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
-import Rive from "@rive-app/react-canvas";
 //
 import RiveContainer from "@/components/RiveContainer";
 
--- components/RiveContainer.tsx
+++ components/RiveContainer.tsx
@@ -1,38 +1,13 @@
 "use client";
 import React from "react";
-import Rive, {
-  useRive,
-  Layout,
-  Fit,
-  Alignment,
-} from "@rive-app/react-canvas";
+import Rive from "@rive-app/react-canvas";
 
 export default function RiveContainer() {
-  const { rive, RiveComponent } = useRive({
-    src: "/rocket-animation.riv",
-    autoplay: true,
-    // layout: new Layout({
-    //   fit: Fit.Cover,
-    //   alignment: Alignment.Center,
-    // }),
-    artboard: "New Artboard",
-    stateMachines: ["State Machine 1"],
-  });
-
-  if (rive?.contents) {
-    console.log("Rendering rocket animation");
-    console.log(rive?.animationNames);
-  }
 
   return (
     <div className="flex justify-center">
       <div className="h-[300px] w-[300px] xmd:h-[450px] xmd:w-[450px] md:h-[500px] md:w-[500px]">
-        <RiveComponent
-          onMouseEnter={() => rive && rive.play()}
-          onMouseLeave={() =>
-            rive && rive.pause()
-          }
-        />
+        <Rive src="rocket-animation.riv" />
       </div>
     </div>
   );
