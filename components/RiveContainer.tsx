bcfabbd61a924403fcfc4e72b26ff6465a718faf
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-03 00:09:37 +0500

    FEAT: Added our favourite animated masked background

--- components/RiveContainer.tsx
+++ components/RiveContainer.tsx
@@ -7,7 +7,7 @@ export default function RiveContainer() {
   return (
     <div className="flex justify-center">
       <div className="h-[300px] w-[300px] xmd:h-[450px] xmd:w-[450px] md:h-[500px] md:w-[500px]">
-        <Rive src="rocket-animation.riv" />
+        <Rive src="rocket-animation.riv"/>
       </div>
     </div>
   );
