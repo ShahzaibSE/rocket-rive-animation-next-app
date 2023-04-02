5612bb8d88c6b08d3dc06435494f2d51bdc3475c
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-03 00:57:54 +0500

    FEAT #3: Triggering animation using "useRive" hook

--- components/RiveContainer.tsx
+++ components/RiveContainer.tsx
@@ -1,13 +1,24 @@
 "use client";
 import React from "react";
-import Rive from "@rive-app/react-canvas";
+import Rive, {
+  useRive,
+  useStateMachineInput,
+} from "@rive-app/react-canvas";
 
 export default function RiveContainer() {
+  const { rive, RiveComponent } = useRive({
+    src: "rocket-animation.riv",
+    artboard: "flying-rocket",
+    autoplay: true,
+  });
+
+  // const trigger = useStateMachineInput(rive, "State Machine 1", "State Machine 1");
 
   return (
     <div className="flex justify-center">
       <div className="h-[300px] w-[300px] xmd:h-[450px] xmd:w-[450px] md:h-[500px] md:w-[500px]">
-        <Rive src="rocket-animation.riv"/>
+        {/* <Rive src="rocket-animation.riv"/> */}
+        <RiveComponent />
       </div>
     </div>
   );
